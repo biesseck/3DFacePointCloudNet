46cf6fe5c9d36927d7b4493504f5064be037b55a
Bernardo Biesseck <biesseck@gmail.com>
2022-09-17 00:10:39 -0300

    Update 'pointnet2/utils/pointnet2_utils.py': replace 'pointnet2._ext' by 'pointnet2_ops._ext'

--- pointnet2/utils/pointnet2_utils.py
+++ pointnet2/utils/pointnet2_utils.py
@@ -17,7 +17,8 @@ except:
     import __builtin__ as builtins
 
 try:
-    import pointnet2._ext as _ext
+    import pointnet2_ops._ext as _ext # BERNARDO
+    # import pointnet2._ext as _ext   # original
 except ImportError:
     if not getattr(builtins, "__POINTNET2_SETUP__", False):
         raise ImportError(
