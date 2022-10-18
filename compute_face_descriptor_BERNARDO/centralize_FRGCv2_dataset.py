a5b9fa9d0e68a7dbce4bd1a146e3917029c65904
Bernardo Biesseck <biesseck@gmail.com>
2022-10-18 09:45:30 -0300

    centralize_FRGCv2_dataset.py: catch exception when face isn't detect in image

--- compute_face_descriptor_BERNARDO/centralize_FRGCv2_dataset.py
+++ compute_face_descriptor_BERNARDO/centralize_FRGCv2_dataset.py
@@ -5,6 +5,9 @@ import argparse
 from pathlib import Path
 import glob
 
+os.environ["CUDA_VISIBLE_DEVICES"]='-1'   # cpu
+# os.environ["CUDA_VISIBLE_DEVICES"]='0'  # gpu
+
 import pcl
 import pcl.pcl_visualization
 from mtcnn import MTCNN
@@ -18,9 +21,10 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument("-dataset_path", type=str, default='/home/bjgbiesseck/datasets/FRGCv2.0/FRGC-2.0-dist/nd1', help='')
-    parser.add_argument('-input_img_ext', type=str, default='*.ppm', help='')
-    parser.add_argument('-input_pc_ext', type=str, default='*.abs.gz', help='')
-    parser.add_argument('-output_pc_ext', type=str, default='*.xyz', help='')
+    parser.add_argument('-input_img_ext', type=str, default='.ppm', help='')
+    parser.add_argument('-input_pc_ext', type=str, default='.abs.gz', help='')
+    parser.add_argument('-output_pc_ext', type=str, default='_centralized_nosetip.ply', help='')
+    parser.add_argument('-start_from', type=str, default='', help='')
 
     return parser.parse_args()
 
@@ -53,7 +57,7 @@ class TreeFRGCv2:
         # print('all_sub_folders:', all_sub_folders)
         valid_sub_folders = []
         for sub_folder in all_sub_folders:
-            files = glob.glob(sub_folder + '/' + pc_ext)
+            files = glob.glob(sub_folder + '/*' + pc_ext)
             if len(files) > 0:
                 valid_sub_folders.append(sub_folder)
                 # print('sub_folder:', sub_folder)
@@ -61,8 +65,8 @@ class TreeFRGCv2:
         all_img_paths = []
         all_pc_paths = []
         for valid_sub_folder in valid_sub_folders:
-            img_paths = sorted(glob.glob(valid_sub_folder + '/' + img_ext))
-            pc_paths =  sorted(glob.glob(valid_sub_folder + '/' + pc_ext))
+            img_paths = sorted(glob.glob(valid_sub_folder + '/*' + img_ext))
+            pc_paths =  sorted(glob.glob(valid_sub_folder + '/*' + pc_ext))
             
             all_img_paths += img_paths
             all_pc_paths += pc_paths
@@ -80,16 +84,35 @@ def load_imgRGB(img_path=''):
 
 
 def detect_nose_landmarks(detector, imgRGB):
+    try:
+        landmarks = detector.detect_faces(imgRGB)[0]
+        nose_xy = landmarks['keypoints']['nose']
+        nose_index = nose_xy[0] + nose_xy[1]*imgRGB.shape[1]
+        # print('nose_xy:', nose_xy)
+        # imgRGB_with_landmark = cv2.circle(imgRGB.copy(), nose_xy, radius=3, color=(0, 0, 255), thickness=-1)
+        # cv2.imshow('imgRGB_with_landmark', imgRGB_with_landmark)
+        # cv2.imshow('imgRGB', imgRGB)
+        # cv2.waitKey(0)
+        # sys.exit(0)
+        return nose_xy, nose_index
+
+    except IndexError:
+        raise Exception('No face detected in image')
+
+
+'''
+def detect_nose_landmarks(detector, imgRGB):
     landmarks = detector.detect_faces(imgRGB)[0]
     nose_xy = landmarks['keypoints']['nose']
     nose_index = nose_xy[0] + nose_xy[1]*imgRGB.shape[1]
     # print('nose_xy:', nose_xy)
-    # imgRGB_with_landmark = cv2.circle(imgRGB.copy(), nose, radius=3, color=(0, 0, 255), thickness=-1)
+    # imgRGB_with_landmark = cv2.circle(imgRGB.copy(), nose_xy, radius=3, color=(0, 0, 255), thickness=-1)
     # cv2.imshow('imgRGB_with_landmark', imgRGB_with_landmark)
     # cv2.imshow('imgRGB', imgRGB)
     # cv2.waitKey(0)
     # sys.exit(0)
     return nose_xy, nose_index
+'''
 
 
 def load_point_cloud(path_point_cloud):
@@ -152,8 +175,13 @@ def show_point_cloud(cloud):
     viewer.Spin()
 
 
-def save_normalized_point_cloud(ptcloud_centralized, path_centralized_ptcloud):
-    pcl.sa
+
+def find_index_of_file_name(paths_list, file_name_search):
+    for i in range(len(paths_list)):
+        if file_name_search in paths_list[i]:
+            return i
+    return 0
+
 
 
 def main_normalize(args):
@@ -165,24 +193,37 @@ def main_normalize(args):
     print('Initializing MTCNN face detector...')
     detector = MTCNN()
 
-    for i in range(len(img_paths)):
+    # start_index = find_index_of_file_name(pc_paths, args.start_from)
+    start_index = find_index_of_file_name(img_paths, args.start_from)
+
+    for i in range(start_index, len(img_paths)):
         img_path = img_paths[i]
         pc_path = pc_paths[i]
-        print('pc_path:', pc_path)
-        print('img_path:', img_path)
-        print('---------------------')
+        print(str(i) + '/' + str(len(img_paths)) + ' - pc_path:', pc_path)
+        print(str(i) + '/' + str(len(img_paths)) + ' - img_path:', img_path)
 
         imgRGB = load_imgRGB(img_path)
         print('Detecting face landmarks...')
-        nose_landmark_xy, nose_landmark_index = detect_nose_landmarks(detector, imgRGB)
 
-        ptcloud, _ = load_point_cloud(pc_path)
-        ptcloud_centralized = centralize_and_filter_pointcloud(ptcloud, nose_landmark_index)
-        # show_point_cloud(ptcloud_centralized)
+        try:
+            nose_landmark_xy, nose_landmark_index = detect_nose_landmarks(detector, imgRGB)
 
-        path_centralized_ptcloud = '/'.join(pc_path.split('/')[:-1]) + '/' + pc_path.split('/')[-1].split('.')[0] + args.output_pc_ext
-        print('Saving centralized point cloud:', path_centralized_ptcloud)
-        pcl.save(ptcloud_centralized, path_centralized_ptcloud, format=args.output_pc_ext.split('.')[1], binary=False)
+            ptcloud, _ = load_point_cloud(pc_path)
+            ptcloud_centralized = centralize_and_filter_pointcloud(ptcloud, nose_landmark_index)
+            # show_point_cloud(ptcloud_centralized)
+
+            path_centralized_ptcloud = '/'.join(pc_path.split('/')[:-1]) + '/' + pc_path.split('/')[-1].split('.')[0] + args.output_pc_ext
+            print('Saving centralized point cloud:', path_centralized_ptcloud)
+            pcl.save(ptcloud_centralized, path_centralized_ptcloud, format=args.output_pc_ext.split('.')[1], binary=False)
+
+            path_imgRGB_with_nose_landmark = img_path.replace(args.input_img_ext, args.output_pc_ext.replace('.ply', '.jpg'))
+            imgRGB_with_landmark = cv2.circle(cv2.cvtColor(imgRGB, cv2.COLOR_RGB2BGR).copy(), nose_landmark_xy, radius=3, color=(0, 0, 255), thickness=-1)
+            print('Saving img RGB with nose landmark:', path_imgRGB_with_nose_landmark)
+            cv2.imwrite(path_imgRGB_with_nose_landmark, imgRGB_with_landmark)
+
+        except:
+            print('No face detected in ' + img_path)
+
         print('-----------------------------------------\n')
         # sys.exit(0)
 
@@ -194,10 +235,12 @@ if __name__ == '__main__':
     if not '-dataset_path' in sys.argv:
         sys.argv += ['-dataset_path', '/home/bjgbiesseck_home_duo/datasets/FRGCv2.0/FRGC-2.0-dist/nd1']
 
-    sys.argv += ['-input_img_ext', '*.ppm']
-    sys.argv += ['-input_pc_ext', '*.abs.gz']
+    sys.argv += ['-input_img_ext', '.ppm']
+    sys.argv += ['-input_pc_ext', '.abs.gz']
 
     sys.argv += ['-output_pc_ext', '_centralized_nosetip.ply']
+
+    # sys.argv += ['-start_from', 'Fall2003range/04729d25.ppm']
 
 
     args = parse_args()
